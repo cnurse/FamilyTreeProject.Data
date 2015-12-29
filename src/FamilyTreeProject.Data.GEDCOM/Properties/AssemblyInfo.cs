91702c55ade5f1055c768c6eb26fff394e8cb82d
Charles Nurse <charlesnurse@gmail.com>
2015-12-29 14:50:24 -0800

    Update to version 0.2 and restore Nuget dependencies

--- src/FamilyTreeProject.Data.GEDCOM/Properties/AssemblyInfo.cs
+++ src/FamilyTreeProject.Data.GEDCOM/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Naif.Data.GEDCOM")]
+[assembly: AssemblyTitle("FamilyTreeProject.Data.GEDCOM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Naif.Data.GEDCOM")]
+[assembly: AssemblyProduct("FamilyTreeProject.Data.GEDCOM")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,15 +18,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
--- tests/FamilyTreeProject.TestUtilities/AssemblyInfo.cs
+++ tests/FamilyTreeProject.TestUtilities/AssemblyInfo.cs
@@ -11,24 +11,35 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("FamilyTreeProject.Data.GEDCOM.Tests")]
+[assembly: AssemblyTitle("FamilyTreeProject.Tests.Utility")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FamilyTreeProject.Data.GEDCOM.Tests")]
+[assembly: AssemblyProduct("FamilyTreeProject.Tests.Utility")]
 [assembly: AssemblyCopyright("Copyright ©  2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM componenets.  If you need to access a type in this assembly from 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
-[assembly: Guid("3e7a6276-ec6c-4bcd-b582-74befba3a729")]
+[assembly: Guid("10e6c063-0851-4a81-a927-cefd931f9222")]
 
-[assembly: AssemblyVersion("0.2.0")]
-[assembly: AssemblyFileVersion("0.2.0")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
